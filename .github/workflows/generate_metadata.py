4c5d623e78c038a306ccaf3c1792b9a495cb52b5
Xavier Mitault <xavier.mitault@epitech.eu>
2024-02-11 14:18:48 +0100

    Show error package metas

--- .github/workflows/generate_metadata.py
+++ .github/workflows/generate_metadata.py
@@ -2,6 +2,7 @@ import tomllib
 import os
 import requests
 import zipfile
+import pathlib
 from github import Github
 
 g = Github()
@@ -10,7 +11,35 @@ PACKAGES_FILE = "/tmp/paq-packages.toml"
 with open(PACKAGES_FILE, "w") as f:
     f.write("[paq]\n\n")
 
+LOG_FILE = "/tmp/paq-packages.log"
+with open(LOG_FILE, "w") as f:
+    f.write("")
+
 packages = g.get_repo("Saverio976/paq").get_latest_release().get_assets()
+
+def log_error(message):
+    with open(LOG_FILE, "a") as f:
+        f.write(message + "\n")
+
+def verify_metadata(data):
+    if "author" not in data or not isinstance(data["author"], str):
+        return False
+    if "description" not in data or not isinstance(data["description"], str):
+        return False
+    if "homepage" not in data or not isinstance(data["homepage"], str):
+        return False
+    if "license" not in data or not isinstance(data["license"], str):
+        return False
+    if "binaries" not in data or not isinstance(data["binaries"], list):
+        return False
+    for binary in data["binaries"]:
+        if not isinstance(binary, str):
+            return False
+    if "name" not in data or not isinstance(data["name"], str):
+        return False
+    if "version" not in data or not isinstance(data["version"], str):
+        return False
+    return True
 
 for package in packages:
     print(package.name, "...")
@@ -25,7 +54,13 @@ for package in packages:
             with zipp.open("metadata.toml") as meta:
                 data = tomllib.load(meta)
     except Exception as esc:
-        print(f"Error in package {package.name}: {esc}")
+        log_error(f"Error in package {package.name}: {esc}")
+        continue
+    if not verify_metadata(data):
+        log_error(f"Error in package {package.name}: invalid metadata")
+        continue
+    if data["name"] != pathlib.Path(package.name).stem:
+        log_error(f"Error in package {package.name}: invalid name")
         continue
     with open(PACKAGES_FILE, "a") as f:
         f.write(f"[packages.{data['name']}]\n")
