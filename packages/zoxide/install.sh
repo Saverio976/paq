0c4035d932c6fdb83638f16b16dd44436925cf24
Xavier Mitault <xavier.mitault@epitech.eu>
2024-02-14 13:21:39 +0100

    Fix zoxide package

--- packages/zoxide/install.sh
+++ packages/zoxide/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/ajeetdsouza/zoxide/releases/latest' | cut -d'/' -f8)
+if [[ "${version}" == "v"* ]]; then
+    version=${version:1}
+fi
 curl -Lo /tmp/zoxide.tar.gz "https://github.com/ajeetdsouza/zoxide/releases/latest/download/zoxide-${version}-x86_64-unknown-linux-musl.tar.gz"
 tar xf /tmp/zoxide.tar.gz -C /tmp
