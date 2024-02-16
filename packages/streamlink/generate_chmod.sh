ae0b75e6e0eb59ce212fde6f602c92709c4f7db3
Xavier Mitault <xavier.mitault@epitech.eu>
2024-02-16 22:31:13 +0100

    Fix binary for streamlink

---
+++ packages/streamlink/generate_chmod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "chmod = [" >> /tmp/out/metadata.toml
+
+for file in /tmp/out/usr/bin/*; do
+    true_file="usr/bin/$(basename "$file")"
+    echo "    { \"path\": \"${true_file}\", \"mode\": \"binary\" }," >> /tmp/out/metadata.toml
+done
+
+echo "]" >> /tmp/out/metadata.toml
