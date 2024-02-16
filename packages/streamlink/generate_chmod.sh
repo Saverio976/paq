a0d68082f0ce10bc0df4ba4f2b34c78070a83ac4
Xavier Mitault <xavier.mitault@epitech.eu>
2024-02-16 22:50:27 +0100

    Fix metadata for streamlink

--- packages/streamlink/generate_chmod.sh
+++ packages/streamlink/generate_chmod.sh
@@ -4,7 +4,7 @@ echo "chmod = [" >> /tmp/out/metadata.toml
 
 for file in /tmp/out/usr/bin/*; do
     true_file="usr/bin/$(basename "$file")"
-    echo "    { \"path\": \"${true_file}\", \"mode\": \"binary\" }," >> /tmp/out/metadata.toml
+    echo "    { \"path\" = \"${true_file}\", \"mode\" = \"binary\" }," >> /tmp/out/metadata.toml
 done
 
 echo "]" >> /tmp/out/metadata.toml
