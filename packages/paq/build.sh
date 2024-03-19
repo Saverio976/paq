c066e8a2275aa050d428ca3c66ad6e196f19ed36
Xavier Mitault <xavier.mitault@epitech.eu>
2024-03-19 08:48:49 +0100

    Use pyinstaller instead of nuitka

--- packages/paq/build.sh
+++ packages/paq/build.sh
@@ -3,11 +3,13 @@ set -ex
 
 pdm install
 # build
-pdm run build
+pdm run build-pyinstaller
+#pdm run build-nuitka
 
 # generate files packaged to a temporary directory
 mkdir -p /tmp/out/bin
-cp ./paq.bin /tmp/out/bin/paq
+#cp ./paq.bin /tmp/out/bin/paq
+cp ./dist/paq /tmp/out/bin/paq
 
 # copy to /out
 cp -r /tmp/out/* /out
