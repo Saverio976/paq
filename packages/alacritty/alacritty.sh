49312537ee9d2ebe54f25dee3bad94f766a0a605
Xavier Mitault <xavier.mitault@epitech.eu>
2024-03-20 16:22:58 +0100

    Remove cd in the package

--- packages/alacritty/alacritty.sh
+++ packages/alacritty/alacritty.sh
@@ -7,8 +7,7 @@ PATH_BIN=$(dirname "$(readlink -f "$0")")
 PATH_BIN=$(dirname "$PATH_BIN")
 
 (
-    cd "$PATH_BIN"
-    export LD_LIBRARY_PATH="$PATH_BIN/libs:$LD_LIBRARY_PATH"
     tic -xe alacritty,alacritty-direct "$PATH_BIN/alacritty.info"
-    exec ./alacritty
+    export LD_LIBRARY_PATH="$PATH_BIN/libs:$LD_LIBRARY_PATH"
+    exec $PATH_BIN/alacritty
 )
