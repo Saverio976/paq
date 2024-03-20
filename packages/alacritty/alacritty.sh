a38b664a84608a1d33cd91d9166af9e11facb974
Xavier Mitault <xavier.mitault@epitech.eu>
2024-03-20 16:34:33 +0100

    Add exec of args to alacritty

--- packages/alacritty/alacritty.sh
+++ packages/alacritty/alacritty.sh
@@ -3,11 +3,13 @@
 # alacritty/bin
 PATH_BIN=$(dirname "$(readlink -f "$0")")
 
+ARGS=${@:1}
+
 # alacritty
 PATH_BIN=$(dirname "$PATH_BIN")
 
 (
     tic -xe alacritty,alacritty-direct "$PATH_BIN/alacritty.info"
     export LD_LIBRARY_PATH="$PATH_BIN/libs:$LD_LIBRARY_PATH"
-    exec $PATH_BIN/alacritty
+    exec $PATH_BIN/alacritty ${ARGS}
 )
