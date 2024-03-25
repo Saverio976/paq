3281ef4071ceeb63d3b7a9d1a810dfc7b5912f65
Xavier Mitault <xavier.mitault@epitech.eu>
2024-03-25 22:11:55 +0000

    Remove cache

--- .github/workflows/generate_package.sh
+++ .github/workflows/generate_package.sh
@@ -32,20 +32,6 @@ build_package() {
     rm -rf "$TMP_DIR"
     rm -rf "$TMP_ZIP"
     rm -rf "$TMP_ZIP"
-    # add cache for paq because it is slow to build paq itself
-    # yeah i know, 'rewrite it in rust', "don't try to 'compile' python"
-    DOCKER_ARGS_PAQ=""
-    if [[ "$FILE" == "./packages/paq" ]]; then
-        mkdir -p /tmp/cache-paq/build
-        mkdir -p /tmp/cache-paq/dist
-        mkdir -p /tmp/cache-paq/onefile-build
-        mkdir -p /tmp/cache-paq/venv
-        DOCKER_ARGS_PAQ="-v /tmp/cache-paq/build:/paq/paq.build"
-        DOCKER_ARGS_PAQ="$DOCKER_ARGS_PAQ -v /tmp/cache-paq/dist:/paq/paq.dist"
-        DOCKER_ARGS_PAQ="$DOCKER_ARGS_PAQ -v /tmp/cache-paq/onefile-build:/paq/paq.onefile-build"
-        DOCKER_ARGS_PAQ="$DOCKER_ARGS_PAQ -v /tmp/cache-paq/venv:/paq/.venv"
-    fi
-    #--
     (
         exec 3>&1 4>&2 > "$LOG_FILE" 2>&1
         set -ex
@@ -56,7 +42,6 @@ build_package() {
         docker run \
             --rm \
             -v "$TMP_DIR:/out" \
-            $DOCKER_ARGS_PAQ \
             "package-tmp-$(basename "$FILE")"
         (cd "$TMP_DIR" && zip -r "$TMP_ZIP" . || true)
         cp "$TMP_ZIP" "$TARGET_ZIP"
