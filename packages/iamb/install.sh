1b8e2a26962142ef139263af50682d20407ae7f9
Xavier Mitault <xavier.mitault@epitech.eu>
2024-04-04 15:30:53 +0200

    Add iamb

---
+++ packages/iamb/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/ulyssa/iamb/releases/latest' | cut -d'/' -f8)
+curl -Lo /tmp/iamb.tar.gz "https://github.com/ulyssa/iamb/releases/latest/download/iamb-${version}-x86_64-unknown-linux-musl.tgz"
+tar xf /tmp/iamb.tar.gz -C /tmp
+mv "/tmp/iamb-${version}-x86_64-unknown-linux-musl" /tmp/iamb
