489d4bbd45b8d7265645a05117ddcd99f3da2bc4
Xavier Mitault <xavier.mitault@epitech.eu>
2024-04-04 15:50:11 +0200

    Fix perms

--- packages/iamb/install.sh
+++ packages/iamb/install.sh
@@ -4,3 +4,4 @@ version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/ulyssa/i
 curl -Lo /tmp/iamb.tar.gz "https://github.com/ulyssa/iamb/releases/latest/download/iamb-${version}-x86_64-unknown-linux-musl.tgz"
 tar xf /tmp/iamb.tar.gz -C /tmp
 mv "/tmp/iamb-${version}-x86_64-unknown-linux-musl" /tmp/iamb
+find /tmp/iamb -type f -exec chmod a+rwx {} \;
