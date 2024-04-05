fd81b3db4307b07c6cf2e461f490602253005e97
Xavier Mitault <xavier.mitault@epitech.eu>
2024-04-05 14:09:35 +0200

    Add lib

--- packages/wezterm/generate_chmod.sh
+++ packages/wezterm/generate_chmod.sh
@@ -7,4 +7,9 @@ for file in /tmp/out/usr/bin/*; do
     echo "    { \"path\" = \"${true_file}\", \"mode\" = \"binary\" }," >> /tmp/out/metadata.toml
 done
 
+for file in /tmp/out/usr/lib/*.so.*; do
+    true_file="usr/lib/$(basename "$file")"
+    echo "    { \"path\" = \"${true_file}\", \"mode\" = \"binary\" }," >> /tmp/out/metadata.toml
+done
+
 echo "]" >> /tmp/out/metadata.toml
