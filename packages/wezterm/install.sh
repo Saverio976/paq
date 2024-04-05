bb1a4dffa0210f418329655d1642483723923d09
Xavier Mitault <xavier.mitault@epitech.eu>
2024-04-05 13:53:54 +0200

    Add wezterm

---
+++ packages/wezterm/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/wez/wezterm/releases/latest' | cut -d'/' -f8)
+curl -Lo /tmp/wezterm-appimage "https://github.com/wez/wezterm/releases/latest/download/WezTerm-$version-Ubuntu20.04.AppImage"
+chmod +x /tmp/wezterm-appimage
+cd /tmp
+/tmp/wezterm-appimage --appimage-extract
+mv /tmp/squashfs-root /tmp/wezterm
+mv /tmp/wezterm/AppRun /tmp/wezterm/wezterm
