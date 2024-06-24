f3023a1dba4a59c775f96ef4c30b1203023ebe31
Xavier Mitault <xavier.mitault@epitech.eu>
2024-06-24 20:45:37 +0200

    Fix install atuin

--- packages/atuin/install.sh
+++ packages/atuin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/atuinsh/atuin/releases/latest' | cut -d'/' -f8)
-curl -Lo /tmp/atuin.tar.gz "https://github.com/atuinsh/atuin/releases/latest/download/atuin-${version}-x86_64-unknown-linux-musl.tar.gz"
+#version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/atuinsh/atuin/releases/latest' | cut -d'/' -f8)
+curl -Lo /tmp/atuin.tar.gz "https://github.com/atuinsh/atuin/releases/latest/download/atuin-x86_64-unknown-linux-musl.tar.gz"
 tar xf /tmp/atuin.tar.gz -C /tmp
-mv "/tmp/atuin-${version}-x86_64-unknown-linux-musl" /tmp/atuin
+mv "/tmp/atuin-x86_64-unknown-linux-musl" /tmp/atuin
