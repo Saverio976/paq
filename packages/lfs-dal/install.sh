a76f83357a03a2d7a41b53c93d12075fe45ce5c3
Xavier Mitault <xavier.mitault@epitech.eu>
2024-07-01 10:43:39 +0200

    Add lfs-dal package

---
+++ packages/lfs-dal/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+version=$(curl -Ls -o /dev/null -w %{url_effective} 'https://github.com/regen100/lfs-dal/releases/latest' | cut -d'/' -f8)
+curl -Lo /tmp/lfs-dal.tar.gz "https://github.com/regen100/lfs-dal/releases/latest/download/lfs-dal-${version}-x86_64-unknown-linux-musl.tar.gz"
+tar xf /tmp/lfs-dal.tar.gz -C /tmp
+mv "/tmp/lfs-dal-${version}-x86_64-unknown-linux-musl" /tmp/lfs-dal
